08d33448d81db685c7c9dc308955ebc0bc6731bd
MWlodarz <wlodarzmar@gmail.com>
2020-09-23 21:46:12 +0200

    Wealth model migration added

---
+++ MoneyMonitor/Data/Migrations/20200923194511_WealthModelAdded.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoneyMonitor.Data.Migrations
+{
+    public partial class WealthModelAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Wealths",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 255, nullable: false),
+                    OwnerId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wealths", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Wealths_AspNetUsers_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wealths_OwnerId",
+                table: "Wealths",
+                column: "OwnerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Wealths");
+        }
+    }
+}
