e5fbd4690d1ead7332eb2575d09cb2dbe46a9cb6
MWlodarz <wlodarzmar@gmail.com>
2020-09-24 21:22:03 +0200

    added migration

--- MoneyMonitor/Data/Migrations/20200924192121_added_year_model.Designer.cs
+++ MoneyMonitor/Data/Migrations/20200924192121_added_year_model.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MoneyMonitor.Data;
 
 namespace MoneyMonitor.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200924192121_added_year_model")]
+    partial class added_year_model
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MoneyMonitor/Data/Migrations/20200924192121_added_year_model.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoneyMonitor.Data.Migrations
+{
+    public partial class added_year_model : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Years",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    WealthId = table.Column<int>(nullable: false),
+                    Order = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Years", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Years_Wealths_WealthId",
+                        column: x => x.WealthId,
+                        principalTable: "Wealths",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Years_WealthId",
+                table: "Years",
+                column: "WealthId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Years");
+        }
+    }
+}
