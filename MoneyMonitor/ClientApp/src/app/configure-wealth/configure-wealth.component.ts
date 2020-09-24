4451f4b3f4f7a241416ca7e2d5f272cfc20b215b
MWlodarz <wlodarzmar@gmail.com>
2020-09-24 07:38:38 +0200

    get the wealth id in configure  wealth component

--- MoneyMonitor/ClientApp/src/app/configure-wealth/configure-wealth.component.ts
+++ MoneyMonitor/ClientApp/src/app/configure-wealth/configure-wealth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Wealth } from '../services/wealth.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-configure-wealth',
@@ -11,8 +12,12 @@ export class ConfigureWealthComponent {
 
   public wealth: Wealth;
 
-  constructor() {
+  constructor(private route: ActivatedRoute) {
     this.wealth = new Wealth();
     this.wealth.name = 'wealth name';
+    route.paramMap.subscribe((params) => {
+      let wealthId = parseInt(params.get('id'));
+      console.log(wealthId, 'ID');
+    });
   }
 }
